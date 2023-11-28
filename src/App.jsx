bdf06867d95540e103b9c0d360eb1e5b58f8953c
shotapailodze <shotikophailodze1998@gmail.com>
2023-11-28 14:10:35 +0400

    home component done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;

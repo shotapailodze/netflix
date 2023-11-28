e0f03a76852ce1af68165cfe8dffc687a8446305
shotapailodze <shotikophailodze1998@gmail.com>
2023-11-28 20:02:09 +0400

    authetication done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Account from "./pages/Account.jsx";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
@@ -11,6 +15,16 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </AuthContextProvider>
     </>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { user, logIn } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await logIn(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
+  };
+  return (
+    <div className="w-full h-screen ">
+      <img
+        className="hidden sm:block absolute w-full h-full object-cover"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/301b666a-a3f6-4aed-a56e-1c9872a11d07/GE-en-20231120-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+        alt="/"
+      />
+      <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+      <div className="fixed w-full px-4 py-24 z-50">
+        <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+          <div className="max-w-[320px] mx-auto py-16">
+            <h1 className="text-3xl font-bold">Sign In</h1>
+            {error ? <p className="p-3 bg-red-400 my-2">{error}</p> : null}
+            <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                className="p-3 my-2 bg-gray-700 rounded"
+                type="email"
+                placeholder="Email"
+                autoComplete="email"
+              />
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                className="p-3 my-2 bg-gray-700 rounded"
+                type="password"
+                placeholder="Password"
+                autoComplete="current-password"
+              />
+              <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                Sign In
+              </button>
+              <div className="flex justify-between items-center text-sm text-gray-600">
+                <p>
+                  <input type="checkbox" className="mr-2" />
+                  Remember me
+                </p>
+                <p>Need Help ?</p>
+              </div>
+              <p className="py-8">
+                <span className="text-gray-600">New to Netflix?</span>{" "}
+                <Link to="/signup">Sign Up</Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

e5a762d23dec2fbc0064260574b7d96a5bea4b93
shotapailodze <shotikophailodze1998@gmail.com>
2023-11-28 20:41:29 +0400

    linked to firebase database and added remove movie function from account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import SavedShows from "../components/SavedShows";
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/301b666a-a3f6-4aed-a56e-1c9872a11d07/GE-en-20231120-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
 };
 
 export default Account;
